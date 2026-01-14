2f100d77c2221f90280f8eddd639294633b47941
Muhammed Enes Karaca <eneskaracap@gmail.com>
2026-01-14 20:12:19 +0300

    init project with cmake and test program

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    auto window = sf::RenderWindow(sf::VideoMode::getDesktopMode(), "Cengaver SFML Test");
+    window.setFramerateLimit(60);
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>()) { window.close(); }
+        }
+
+        window.clear();
+        window.display();
+    }
+}
